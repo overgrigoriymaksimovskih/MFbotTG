ca95d087fe1568e1a5f9302998fb76bcd1be587f
overgrigoriymaksimovskih <overbf@gmail.com>
2025-03-26 16:31:03 +0500

    added .env

--- node/src/main/java/pro/masterfood/NodeApplication.java
+++ node/src/main/java/pro/masterfood/NodeApplication.java
@@ -3,9 +3,27 @@ package pro.masterfood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class NodeApplication {
     public static void main(String[] args) {
+        //установка переменных окружения нужна только чтобы запускать из идеи сервис у которого переменные заданы в .env
+        //можно все указать прямо в properties и тогда этот блок не нужен
+
+        // Загрузка переменных окружения из файла .env
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./") // Укажите каталог, где находится .env (по умолчанию текущий каталог)
+                .ignoreIfMalformed() // Игнорировать строки с неверным синтаксисом в .env
+                .ignoreIfMissing() // Игнорировать, если файл .env отсутствует
+                .load();
+
+        // Установка переменных окружения как системных свойств
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        // установка переменных окружения завершена
+
+
+        // Запуск Spring Boot
         SpringApplication.run(NodeApplication.class);
     }
 }
