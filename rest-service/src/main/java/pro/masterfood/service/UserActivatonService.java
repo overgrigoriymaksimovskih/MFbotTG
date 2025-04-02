83254190276f3705be5d18a201aa5390ccf92fb7
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-02 15:01:53 +0500

    test 1

--- rest-service/src/main/java/pro/masterfood/service/UserActivatonService.java
+++ rest-service/src/main/java/pro/masterfood/service/UserActivatonService.java
@@ -2,5 +2,9 @@ package pro.masterfood.service;
 
 public interface UserActivatonService {
     boolean activation(String cryptoUserId);
-    boolean activationMf(String action, String email);
+    boolean activationMf(String action,
+                         String email,
+                         String password,
+                         String check_num,
+                         String token);
 }
