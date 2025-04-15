87089603207bbfe2f0c3b40e6fef1b116ffd6bf9
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-15 14:07:54 +0500

    15.04.25 test login 1.1

--- rest-service/src/main/java/pro/masterfood/service/impl/ConsumerServiceImpl.java
+++ rest-service/src/main/java/pro/masterfood/service/impl/ConsumerServiceImpl.java
@@ -3,21 +3,49 @@ package pro.masterfood.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import pro.masterfood.dao.AppUserDAO;
 import pro.masterfood.dto.LoginParams;
 import pro.masterfood.service.ConsumerService;
 import pro.masterfood.service.UserActivationService;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class ConsumerServiceImpl implements ConsumerService {
 
     private final UserActivationService userActivationService;
+    private final AppUserDAO appUserDAO;
 
     @Override
     @RabbitListener(queues = "${spring.rabbitmq.queues.registration-mail}")
     public void consumeLogin(LoginParams mailParams) {
         String email = mailParams.getEmail();
         String password = mailParams.getPassword();
-        userActivationService.activationFromSite(email, password);
+        var res = userActivationService.activationFromSite(email, password);
+        // 1. Извлекаем Map "isAuthorized"
+        Map<String, Object> isAuthorizedMap = (Map<String, Object>) res.get("isAuthorized");
+
+        // 2. Извлекаем значение "Status" из isAuthorizedMap
+        String status = "failure"; // Значение по умолчанию
+        String message = "Не удалось связаться с сервисом авторизации...";
+        if (isAuthorizedMap != null && isAuthorizedMap.containsKey("Result") && isAuthorizedMap.get("Result") instanceof Map) {
+            Map<?, ?> resultMap = (Map<?, ?>) isAuthorizedMap.get("Result");
+            if (resultMap.containsKey("Status") && resultMap.get("Status") instanceof String) {
+                String statusValue = (String) resultMap.get("Status");
+                if ("success".equalsIgnoreCase(statusValue)) {
+                    status = "success";
+                    var optional = appUserDAO.findById(1L);
+                    if (optional.isPresent()) {
+                        var user = optional.get();
+                        user.setIsActive(true);
+                        appUserDAO.save(user);
+                    }
+                }
+            }
+            if (resultMap.containsKey("Msg") && resultMap.get("Msg") instanceof String) {
+                message = (String) resultMap.get("Msg");
+            }
+        }
     }
 }
