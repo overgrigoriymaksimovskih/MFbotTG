9b51774ec53b9430e64d49e70b54b848482c7005
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-16 15:37:34 +0500

    16.04.25 test login 2.8

--- node/src/main/java/pro/masterfood/service/impl/AppUserServiceImpl.java
+++ node/src/main/java/pro/masterfood/service/impl/AppUserServiceImpl.java
@@ -38,9 +38,11 @@ public class AppUserServiceImpl implements AppUserService {
     public String registerUser(AppUser appUser) {
         if (appUser.getIsActive()){
             return "Вы уже зарегистрированы";
-        } else if (appUser.getEmail() != null){
-            return  "Вам на почту уже было выслано письмо \n"
-                    + " перейдите по ссылке для завершения регистрации";
+        } else if (appUser.getEmail() != null && appUser.getIsActive()){
+            appUser.setState(WAIT_FOR_EMAIL_STATE);
+            appUserDAO.save(appUser);
+            return  "Ваш email " +  appUser.getEmail() +"\n"
+                    + " введите пароль";
         }
         appUser.setState(WAIT_FOR_EMAIL_STATE);
         appUserDAO.save(appUser);
