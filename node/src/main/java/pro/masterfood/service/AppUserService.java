870039673dae216d0c5c946b51d01153ff4fe3c5
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-16 10:37:03 +0500

    Revert "16.04.25 test login 1.7.1"
    This reverts commit a481b8d779f478cd36a56bc6c404e409446b0bed.

--- node/src/main/java/pro/masterfood/service/AppUserService.java
+++ node/src/main/java/pro/masterfood/service/AppUserService.java
@@ -5,5 +5,5 @@ import pro.masterfood.entity.AppUser;
 public interface AppUserService {
     String registerUser(AppUser appUser);
     String setEmail (AppUser appUser, String email);
-    String checkPassword (AppUser appUser, String password);
+    String checkPassword (Long chatId, AppUser appUser, String password);
 }
