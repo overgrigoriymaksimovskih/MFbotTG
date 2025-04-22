27ec9d90289915268e9c6e456e51036902b39f58
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-22 11:40:10 +0500

    22.04.25 test TTL and basicAck 1.0

--- rest-service/src/main/java/pro/masterfood/service/ConsumerService.java
+++ rest-service/src/main/java/pro/masterfood/service/ConsumerService.java
@@ -2,7 +2,11 @@ package pro.masterfood.service;
 
 import pro.masterfood.dto.RequestParams;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.messaging.handler.annotation.Headers;
+import java.util.Map;
+
 public interface ConsumerService {
 
-    void consumeRequestToREST(RequestParams requestParams);
+    void consumeRequestToREST(RequestParams requestParams, Channel channel, @Headers Map<String, Object> headers);
 }
--- rest-service/src/main/java/pro/masterfood/service/impl/ConsumerServiceImpl.java
+++ rest-service/src/main/java/pro/masterfood/service/impl/ConsumerServiceImpl.java
@@ -9,6 +9,15 @@ import pro.masterfood.service.ConsumerService;
 import pro.masterfood.service.UserActivationService;
 import pro.masterfood.service.UserInformationProvider;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.amqp.support.AmqpHeaders;
+import java.util.Map;
+import java.io.IOException;
+
+
+
+
 
 @RequiredArgsConstructor
 @Service
@@ -19,13 +28,27 @@ public class ConsumerServiceImpl implements ConsumerService {
 
     @Override
     @RabbitListener(queues = "${spring.rabbitmq.queues.login}")
-    public void consumeRequestToREST(RequestParams requestParams){
-        if(RequestsToREST.LOGIN_REQUEST.equals(requestParams.getRequestType())){
-            userActivationService.consumeLogin(requestParams);
-        } else if (RequestsToREST.PRESENTS_REQUEST.equals(requestParams.getRequestType())) {
-            userInformationProvider.consumeGetBalance(requestParams);
-        } else if (RequestsToREST.ORDER_STATUS_REQUEST.equals(requestParams.getRequestType())) {
-            userInformationProvider.consumeGetOrderStatus(requestParams);
+    public void consumeRequestToREST(RequestParams requestParams, Channel channel, @Headers Map<String, Object> headers){
+
+        Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+
+        try {
+            if(RequestsToREST.LOGIN_REQUEST.equals(requestParams.getRequestType())){
+                userActivationService.consumeLogin(requestParams);
+            } else if (RequestsToREST.PRESENTS_REQUEST.equals(requestParams.getRequestType())) {
+                userInformationProvider.consumeGetBalance(requestParams);
+            } else if (RequestsToREST.ORDER_STATUS_REQUEST.equals(requestParams.getRequestType())) {
+                userInformationProvider.consumeGetOrderStatus(requestParams);
+            }
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            System.err.println("Error processing message: " + e.getMessage());
+            try {
+                channel.basicNack(deliveryTag, false, false); // Отклоняем сообщение, requeue=false - СООБЩЕНИЕ БУДЕТ УДАЛЕНО
+            } catch (IOException ex) {
+                System.err.println("Error sending Nack: " + ex.getMessage());
+                // В этом месте ничего не можем сделать, ошибка отправки Nack маловероятна
+            }
         }
     }
 }
