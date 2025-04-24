397ad619b9eef241f13d7ec097ea22bb5f42e825
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-24 14:13:12 +0500

    24.04.25 handle photo 1.0.1

--- mail-service/src/main/java/pro/masterfood/service/MailSenderService.java
+++ mail-service/src/main/java/pro/masterfood/service/MailSenderService.java
@@ -1,9 +1,6 @@
 package pro.masterfood.service;
 
-import org.telegram.telegrambots.meta.api.objects.Message;
 import pro.masterfood.dto.MailParams;
-import pro.masterfood.entity.AppPhoto;
-import pro.masterfood.entity.AppUser;
 
 public interface MailSenderService {
     void send (MailParams mailParams);
