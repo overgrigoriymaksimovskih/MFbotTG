0ddd7463025cc55de9060ad971782b6596c3fe4f
overgrigoriymaksimovskih <overbf@gmail.com>
2025-04-24 17:52:57 +0500

    24.04.25 handle photo 1.0.6

--- mail-service/src/main/java/pro/masterfood/service/impl/ConsumerServiceImpl.java
+++ mail-service/src/main/java/pro/masterfood/service/impl/ConsumerServiceImpl.java
@@ -2,13 +2,13 @@ package pro.masterfood.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import pro.masterfood.dto.MailParams;
 import pro.masterfood.service.ConsumerService;
 import pro.masterfood.service.MailSenderService;
 
 @RequiredArgsConstructor
-@Service
+@Component
 public class ConsumerServiceImpl implements ConsumerService {
 
     private final MailSenderService mailSenderService;
