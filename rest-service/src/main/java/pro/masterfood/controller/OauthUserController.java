86d0775b486323f9369495760034e1fdaafb7e3f
overgrigoriymaksimovskih <overbf@gmail.com>
2025-10-01 11:19:55 +0500

    WbAppModify_1.2

--- rest-service/src/main/java/pro/masterfood/controller/OauthUserController.java
+++ rest-service/src/main/java/pro/masterfood/controller/OauthUserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RequestMapping("/web-app")
+@RequestMapping("/api")
 @RestController
 public class OauthUserController {
 
